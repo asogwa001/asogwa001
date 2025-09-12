4d97a89d9b4cce7880f38b3075784bdd7513ef87
Asogwa Emmanuel <easogwa@gitam.in>
2025-09-12 23:58:22 +0100

    attach base path dynamically

--- src/projects/CtcController.tsx
+++ src/projects/CtcController.tsx
@@ -11,10 +11,8 @@ const CtcController: ProjectComponent = () => {
     demoUrl: '',
     githubUrl: 'https://github.com/asogwa001/computed_torque',
     images: [
-      "/assets/project_imgs/robot_1.png",
-      "/assets/project_imgs/robot_2.png",      
-      //'https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=800',
-      //'https://images.pexels.com/photos/3184360/pexels-photo-3184360.jpeg?auto=compress&cs=tinysrgb&w=800'
+        `${import.meta.env.BASE_URL}assets/project_imgs/robot_1.png`,
+        `${import.meta.env.BASE_URL}assets/project_imgs/robot_2.png`,   
     ],
     features: [
       'Written in Simulink and MATLAB',
